7e20c305a5778c923a27fbf52612cf1812b272bd
yuriy.netrebin <yuriy.netrebin@globallogic.com>
2021-08-11 10:53:52 +0300

    updated with new version

--- README.md
+++ README.md
@@ -3,6 +3,31 @@
 
 ####Run the Main class to see the following results:
 
-Average price of product: 8.325371900826445
+####Average price of product:
 
-Share of five star products: 48.289451306742585
+: 560.7058823529412
+
+CN : 449.3111413043478
+
+GB : 127.0
+
+SG : 369.5
+
+US : 128.2258064516129
+
+VE : 4.0
+ 
+####Share of five star products:
+
+: 36.98587614465311
+
+CN : 48.502862272129256
+
+GB : 51.20967741935484
+
+SG : 42.18036529680365
+
+US : 49.57595410326765
+
+VE : 47.61904761904762
+
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,16 +3,29 @@ import csvBeans.SummerDataSetBean;
 import lombok.SneakyThrows;
 
 import java.io.FileReader;
-import java.util.OptionalDouble;
+import java.util.TreeMap;
+import java.util.stream.Collectors;
 
 public class Main {
     @SneakyThrows
     public static void main(String[] args) {
         final FileReader reader = new FileReader(ClassLoader.getSystemResource("test-task_dataset_summer_products.csv").getFile());
         final var beanList = new CsvToBeanBuilder<SummerDataSetBean>(reader).withType(SummerDataSetBean.class).build().parse();
-        beanList.stream().mapToDouble(SummerDataSetBean::getPrice).average().ifPresent(e -> System.out.println("Average price of product: " + e));
-        final long fiveStarSum = beanList.stream().filter(t -> t.getRating_five_count() != null).mapToLong(SummerDataSetBean::getRating_five_count).sum();
-        final long ratingCountSum = beanList.stream().filter(t -> t.getRating_count() != null).mapToLong(SummerDataSetBean::getRating_count).sum();
-        OptionalDouble.of(100 * (double) fiveStarSum / ratingCountSum).ifPresent(value -> System.out.println("Share of five star products: " + value));
+
+        System.out.println("Average price of product:");
+        beanList.stream().filter(t -> t.getRating_five_count() != null)
+                .collect(Collectors.groupingBy(SummerDataSetBean::getOrigin_country,
+                        TreeMap::new,
+                        Collectors.averagingDouble(SummerDataSetBean::getRating_five_count)))
+                .forEach((country, averagePrice) -> System.out.println(country + " : " + averagePrice));
+
+        System.out.println("Share of five star products: ");
+        beanList.stream().filter(e -> e.getRating_five_count() != null && e.getRating_count() != null && e.origin_country != null)
+                .collect(Collectors.groupingBy(SummerDataSetBean::getOrigin_country,
+                        TreeMap::new,
+                        Collectors.teeing(Collectors.summingLong(SummerDataSetBean::getRating_five_count),
+                                Collectors.summingLong(SummerDataSetBean::getRating_count),
+                                (a, b) -> 100 * a.doubleValue() / b)))
+                .forEach((country, fiveStarProducts) -> System.out.println(country + " : " + fiveStarProducts));
     }
 }
